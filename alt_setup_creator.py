b8f372f67f0aea5cd919aa6787f8faf4c7a25854
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2025-06-10 15:07:04 +0200

    update

--- alt_setup_creator.py
+++ alt_setup_creator.py
@@ -2,7 +2,7 @@ import json
 
 
 def make_variation(sm_cf: dict[str, float], parameter: str, var: float, negate: bool = False) -> tuple[str, dict[str, float]]:
-    alt_name = f"{parameter}_{'pos' if not negate else 'neg'}_{var:.0e}"
+    alt_name = f"{parameter}_{'pos' if not negate else 'neg'}_{var:.0e}".replace("-", "m") # need to get rid of - to use as c++ identifier later
     alt_conf = sm_cf.copy()
     alt_conf[parameter] += var if not negate else -1.*var
     return alt_name, alt_conf
