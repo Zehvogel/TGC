27068aebd475c6d0c9b8e12ed7793b84a03eb756
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2025-06-11 08:57:44 +0200

    wrap alt setup handling even more

--- alt_setup_creator.py
+++ alt_setup_creator.py
@@ -1,26 +1,51 @@
 import json
 
 
-def make_variation(sm_cf: dict[str, float], parameter: str, var: float, negate: bool = False) -> tuple[str, dict[str, float]]:
-    alt_name = f"{parameter}_{'pos' if not negate else 'neg'}_{var:.0e}".replace("-", "m") # need to get rid of - to use as c++ identifier later
-    alt_conf = sm_cf.copy()
-    alt_conf[parameter] += var if not negate else -1.*var
-    return alt_name, alt_conf
+class AltSetupHandler():
+    _sm_ref: dict[str, float]
+    _variations: dict[str, list[float]]
+    _alt_setup: dict[str, dict[str, float]]
+    _mirror: bool
 
 
-def make_alt_configs(sm_cf: dict[str, float], var_cf: dict[str, list[float]], mirror: bool = True):
-    alt_configs = {}
-    for parameter, variations in var_cf.items():
-        for var in variations:
-            alt_name, alt_conf = make_variation(sm_cf, parameter, var)
-            alt_configs[alt_name] = alt_conf
-            if mirror:
-                alt_name, alt_conf = make_variation(sm_cf, parameter, var, negate=True)
+    def __init__(self, path: str, mirror: bool = True):
+        with open(path) as f:
+            conf = json.load(f)
+        self._sm_ref = conf["SM"]
+        self._variations = conf["variations"]
+        self._mirror = mirror
+        self._alt_setup = self._make_alt_configs()
+
+
+    def make_name(self, parameter: str, var: float, negate: bool = False) -> str:
+        # need to get rid of - to use as c++ identifier later
+        alt_name = f"{parameter}_{'pos' if not negate else 'neg'}_{var:.0e}".replace("-", "m")
+        return alt_name
+
+
+    def _make_variation(self, parameter: str, var: float, negate: bool = False) -> tuple[str, dict[str, float]]:
+        alt_name = self.make_name(parameter, var, negate)
+        alt_conf = self._sm_ref.copy()
+        alt_conf[parameter] += var if not negate else -1.*var
+        return alt_name, alt_conf
+
+
+    def _make_alt_configs(self) -> dict[str, dict[str, float]]:
+        alt_configs = {}
+        for parameter, variations in self._variations.items():
+            for var in variations:
+                alt_name, alt_conf = self._make_variation(parameter, var)
                 alt_configs[alt_name] = alt_conf
-    return alt_configs
+                if self._mirror:
+                    alt_name, alt_conf = self._make_variation(parameter, var, negate=True)
+                    alt_configs[alt_name] = alt_conf
+        return alt_configs
 
 
-def load_config(path: str, mirror: bool = True):
-    with open(path) as f:
-        conf = json.load(f)
-    return make_alt_configs(conf["SM"], conf["variations"])
+    def get_alt_setup(self):
+        # would probably be cleaner to return a deepcopy :/
+        return self._alt_setup
+
+
+    def get_variations(self):
+        return self._variations
